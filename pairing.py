261c3ce8ba8b1e651f8a582596a7d0015148ffd7
ThalyaC1 <nathalie.mamain@yahoo.fr>
2024-01-06 01:22:38 +0100

    modif new_round, create_matches, score_recording

--- pairing.py
+++ pairing.py
@@ -1,6 +1,6 @@
 #from tournament import Tournament
 import json
-from registration_players_tourmanent import open_list_tournaments, create_file_folder_name
+from registration_players_tourmanent import open_list_tournaments, create_file_folder_name, seek_player_ffe
 import random
 
 
@@ -61,8 +61,8 @@ def create_list_players_tournament(tournament_register1):
     with open('chess_data/'+tournament_register_case+tournament_register_players+'.json', 'r') as lp:
         list_chessplayers_tournament = json.load(lp)
         number_players = len(list_chessplayers_tournament)
-        print("nombre de joueurs inscrits au tounoi :", len(list_chessplayers_tournament))
-        print(list_chessplayers_tournament)
+        print("1. nombre de joueurs inscrits au tounoi :", len(list_chessplayers_tournament))
+        print("2.",list_chessplayers_tournament)
     
     return list_chessplayers_tournament, number_players
 
@@ -92,11 +92,20 @@ def new_round():
     info_round={"Nom du tournoi" : round.name_tournament, "Nombre de rounds" : round.number_of_rounds, "Round": round.number_round, "Date de lancement":round.start_date_round, "Heure de lancement":round.start_hour, "Date de fin":round.end_date_round, "Fin de la round":round.end_hour, "Liste des appariements":round.list_matches, "Liste des joueurs":round.list_players_round}
 
     """Enregistrement des infos de la round dans le fichier round du tournoi"""
-    list_rounds=[]
-    list_rounds.append(info_round)
+    #list_rounds=[]
+    #list_rounds.append(info_round)
 
     tourmanent_rounds_file = 'rounds_'+tournament_register1
     tourmanent_register_case = tournament_register1+'/'
+
+    try :
+        with open('chess_data/'+tourmanent_register_case+ tourmanent_rounds_file+'.json', 'r') as lp:
+            list_rounds = json.load(lp)
+       
+    except:
+        list_rounds=[]
+
+    list_rounds.append(info_round)
 
     with open('chess_data/'+tourmanent_register_case+ tourmanent_rounds_file+'.json', 'a', encoding='utf-8') as lp:
         """Attention, pour le moment, ne vérifie pas si la round est djà existante!!!"""
@@ -106,7 +115,33 @@ def new_round():
         
     return number_round, tournament_register1
 
-
+"""
+def create_pair(list_ffe_round, list_players_round, number_tables):
+    n = 1
+    list_pairing=[]
+    for i in range(0, len(list_ffe_round),2):
+        if i + 1 < len(list_ffe_round):
+            new_table = "table"+str(n)
+            list_pairing.append(("Blanc :"+str(list_ffe_round[i]), "Noir :"+str(list_ffe_round[i+1]),new_table))
+        else:
+            list_pairing.append(("Blanc :"+str(list_ffe_round[i]), "Noir : exempt","table"+str(n), "score :0.5-_"))
+            player1 = list_ffe_round[i]
+            print("player1",player1)
+            score_player1_3 = 0.5
+            key_score = "score"
+            for player_round in list_players_round:
+                if key_score in player_round:
+                    player_exempt = player_round
+                    score_player1_2 = float(player_round["score"])
+                    player_round["score"] = score_player1_2 + score_player1_3
+                    break
+            print("joueur exempt:",player_exempt)
+        n += 1
+        if n > number_tables:
+            break
+    return list_pairing
+        #return list_pairing, round_int
+"""
 
 def create_matches(number_round, number_of_rounds, number_players, list_players_round):
     #round = round.number_round
@@ -119,24 +154,44 @@ def create_matches(number_round, number_of_rounds, number_players, list_players_
     list_ffe_round =[]
     list_pairing = [] 
     list_ffe_round = [element["Numero FFE"] for element in list_players_round]
+    print(list_players_round)
     n = 1
     
     if round_int==1:
         """Creation des appariements de la premiere round de manière aléatoire"""        
         random.shuffle(list_ffe_round)
+        #create_pair(list_ffe_round, list_players_round, number_tables)
+        
         for i in range(0, len(list_ffe_round),2):
             if i + 1 < len(list_ffe_round):
                 list_pairing.append(("Blanc :"+list_ffe_round[i], "Noir :"+list_ffe_round[i+1],"table"+str(n)))
             else:
-                list_pairing.append(("Blanc :"+list_ffe_round[i], "Noir : exempt","table"+str(n)))
+                list_pairing.append(("Blanc :"+list_ffe_round[i], "Noir : exempt","table"+str(n), "score :0.5-_"))
+                player1_ffe = list_ffe_round[i]
+                print("player1",player1_ffe)
+                score_player1_3 = 0.5
+                #key_score = "score"
+                key_ffe = "Numero FFE"
+                for player_round in list_players_round:
+                    if key_ffe in player_round and player_round[key_ffe] == player1_ffe:
+                        player_exempt = player_round
+                        score_player1_2 = float(player_round["score"])
+                        player_round["score"] = score_player1_2 + score_player1_3
+                        break
+                print("joueur exempt:",player_exempt)
             n += 1
             if n > number_tables:
                 break
         return list_pairing, round_int
     
-    elif 1< round_int <= number_of_rounds_int:
+    elif 1 < round_int <= number_of_rounds_int:
         """ création des autres rounds en fonction des résultats des joueurs"""
+        #key_score = "score"
+        #list_sorted_by_score = sorted(list_players_round, key=lambda x: x["score"])
+        #create_pair(list_sorted_by_score, list_players_round, number_tables)
+        #return list_pairing, round_int
         pass
+
 
     else :
         print("Cette round n'existe pas")
@@ -156,15 +211,43 @@ def score_recording():
     
     for number_round_score in list_info_current_tournament:
         if number_round_score.get("Round")==number_round_score1:
+
+            """inscription des scores dans la liste d'appariement"""
             list_pairing = number_round_score['Liste des appariements']
             print("2;",list_pairing, len(list_pairing))
             info_pairing = list_pairing[int(table_score2)-1]
             print(info_pairing)
-            add_score1 = input("Entrez le score (1-0,0-1 ou X-X)")
+            add_score1 = input("Entrez le score (1-0,0-1 ou 0.5-0.5)")
             add_score = "Score :"+add_score1
             info_pairing.append(add_score)
             requested_table=info_pairing
             print(requested_table)
+
+            """inscription et modification des scores dans la liste des joueurs"""
+            individual_score = add_score1.split("-")
+            print(individual_score)
+            score_player1_2 = float(individual_score[0])
+            score_player2_2 = float(individual_score[1])
+            print(f"joueur 1: {score_player1_2}, joueur 2: {score_player2_2}")
+            
+            player_1_2 = requested_table[0]
+            player_2_2 = requested_table[1]
+            player_1_1 = player_1_2[-7:]
+            player_2_1 = player_2_2[-7:]
+
+            list_individual = number_round_score['Liste des joueurs']
+            #print(list_individual)
+            player_1 = seek_player_ffe(list_individual, player_1_1)
+            player_2 = seek_player_ffe(list_individual, player_2_1)
+            print(player_1, player_2)
+            
+            score_player1_1 = float(player_1["score"])
+            score_player1 = score_player1_1 + score_player1_2
+            player_1["score"] = score_player1
+            #print(score_player1)
+            score_player2_1 = float(player_2["score"])
+            score_player2 = score_player2_1 + score_player2_2
+            player_2["score"] = score_player2
     
     with open('chess_data/'+tournament_register1+'/rounds_'+tournament_register1+'.json', 'w') as lp:
         json.dump(list_info_current_tournament, lp, indent=4)
@@ -218,8 +301,8 @@ def score_recording():
 #create_matches()
 
 
-#new_round()
+new_round()
 
 #x = 24//2+24%2
 #print(x)
-score_recording()
+#score_recording()
