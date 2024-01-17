c17bb477d4af8afc8ce121f98e8b81527b61ebfe
ThalyaC1 <nathalie.mamain@yahoo.fr>
2024-01-18 00:18:37 +0100

    Pour l'enregistrement d'un joueur : Vérifie que le joueur n'est pas déjà dans le tournoi demandé; résolution d'un bug d'enregistrement d'un joueur. Pour l'affichage à l'écran: ajout du score et affichage par ordre alphabétique

--- registration_players_tourmanent.py
+++ registration_players_tourmanent.py
@@ -56,22 +56,26 @@ def open_list_tournaments():
 
 def register_players_tournament():
     """Objectif : inscrire un joueur à un tournoi.
-    vérifie si à partir du numéro FFE, le joueur est inscrit sur la liste générale du club;
-    Demande à l'utilisateur de sélectionner par le biai d'un numéro le tournoi souhaité;
-    créer si nécessaire le dossier du tournoi et le fichier recevant les joueurs inscrits 
-    pour ce tournoi;
-    enregistre le joueur dans le fichier.
-    Ajoute un score =0
-    !!! pas de vérification pour le moment si l'inscription du joueur dans le fichier existe déjà
+    
+    - Vérifie si le joueur est déjà inscrit au tournoi - si oui fin de la procédure;
+    - sinon à partir du numéro FFE, le joueur est recherché dans la liste générale du club;
+        - si non enregistré : inscription du joueur dans le fichier général;
+        - inscription dans le tournoi choisi :
+            - Demande à l'utilisateur de sélectionner par le biai d'un numéro le tournoi souhaité;
+            - crée si nécessaire le dossier du tournoi et le fichier recevant les joueurs inscrits 
+            pour ce tournoi;
+            - enregistre le numéro FFE du joueur dans le fichier.
+            - Ajoute un score =0
+    
+    Problème : si le joueur n'a jamais été enregistré, l'utilisateur doit saisir 3 fois le n FFE
     """
     
     list_chessplayers = open_list_chessplayers()
     list_tournaments = open_list_tournaments()
     
     list_tournaments_nom = [element["Nom"] for element in list_tournaments]
-
-    ffe_number_register = input("Numéro de licence (du type AB12345) :")
-
+    
+    ffe_number_register = input("Numéro de licence (du type AB12345) tournoi:")
     seek_player = seek_player_ffe(list_chessplayers,ffe_number_register)
 
     if seek_player:
@@ -81,9 +85,15 @@ def register_players_tournament():
         tourmanent_register = 'players_'+tourmanent_register1
         tourmanent_register_case = tourmanent_register1+'/'
         try :
-         with open('chess_data/'+tourmanent_register_case+tourmanent_register+'.json', 'r') as lp:
-            list_players_tournament = json.load(lp)
-       
+            with open('chess_data/'+tourmanent_register_case+tourmanent_register+'.json', 'r') as lp:
+                list_players_tournament = json.load(lp)
+            
+                # Si un joueur est déjà inscrit à ce tournoi
+                seek_player_old = seek_player_ffe(list_players_tournament, ffe_number_register)
+                if seek_player_old :           
+                    print("Ce joueur est déjà enregistré dans ce tournoi")
+                    return None
+                
         except:
             list_players_tournament=[]
        
@@ -94,15 +104,11 @@ def register_players_tournament():
             finally :
                 pass
         
-        """Pour ajouter au profil du joueur un score initial = 0"""
-        
-        player_tournament = {} #
-        player_tournament['Numero FFE'] = seek_player['Numero FFE'] #
+        #Pour ajouter au profil du joueur un score initial = 0
+        player_tournament = {}
+        player_tournament['Numero FFE'] = seek_player['Numero FFE']
         player_tournament ['score'] = 0
-        # score = 'score'
-        # seek_player[score] = 0
 
-        #list_players_tournament.append(seek_player)
         list_players_tournament.append(player_tournament)
 
         with open('chess_data/'+tourmanent_register_case+tourmanent_register+'.json','w', encoding='utf-8') as lp:
@@ -110,9 +116,7 @@ def register_players_tournament():
                 lp.write(',')
             json.dump(list_players_tournament, lp, indent=4)
 
-    else:
-        """print("Personne ne correspond à ce numéro FFE")
-         """
+    else:         
         print("Ce joueur n'est pas enregistré dans la base du club.")
         response = input("Souhaitez-vous l'enregistrer? (Y pour oui/N pour non)")
         if response == "Y":
@@ -120,9 +124,10 @@ def register_players_tournament():
             register_players_tournament()
         else :
             return print("Fin")
-            
+           
 def display_on_screen_players_tournament():
-    """Afficher à l'écran, la liste de joueurs inscrits à un tournoi"""
+    
+    """Afficher à l'écran, par ordre alphabétique, la liste de joueurs inscrits à un tournoi"""
     list_tournaments = open_list_tournaments()
     list_tournaments_nom = [element["Nom"] for element in list_tournaments]
     _, tourmanent_register1 = create_file_folder_name(list_tournaments_nom)
@@ -131,20 +136,22 @@ def display_on_screen_players_tournament():
     with open('chess_data/'+tourmanent_register_case+tourmanent_register+'.json', 'r') as lp:
         list_players_tournament = json.load(lp)
     
+    screen_players = []
     list_players_tournament_ffe = [element['Numero FFE'] for element in list_players_tournament]
     list_chess_players = open_list_chessplayers()
     for player_tournament_ffe1 in list_players_tournament_ffe:
         player_tournament_ffe = seek_player_ffe(list_chess_players, player_tournament_ffe1)
-        # manque le score
+        for player_tournament_ffe1 in list_players_tournament:
+            score1 = player_tournament_ffe1['score']
+                
+        screen_player1 = (player_tournament_ffe["Numero FFE"], player_tournament_ffe["Nom"], player_tournament_ffe["Prenom"], score1)
+        screen_players.append(screen_player1)
+    
+    screen_players_alpha = sorted(screen_players, key=lambda x: x[1])
+
+    for screen_player in screen_players_alpha:
+        print(*screen_player)
         
-        print(player_tournament_ffe["Numero FFE"], player_tournament_ffe["Nom"], player_tournament_ffe["Prenom"])
-        
-        #score_player = player_tournament_ffe1['score']
-        #print(player_tournament_ffe.append(score_player))
-        #print(score_player)
 
-
-
-
-display_on_screen_players_tournament()
+#display_on_screen_players_tournament()
 #register_players_tournament()
