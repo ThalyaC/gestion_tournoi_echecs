eba62100230dbb9f764fe9960ea74e82babddb03
ThalyaC1 <nathalie.mamain@yahoo.fr>
2024-02-13 02:51:07 +0100

    maj

--- src/controller_chess.py
+++ src/controller_chess.py
@@ -1,7 +1,8 @@
 from vues.chessplayers import register_player, display_on_screen_players_club
-from vues.registration_tournament import register_tournament
+from vues.registration_tournament import register_tournament, display_on_screen_tournament_info
 from vues.registration_chessplayers_tourmanent import register_players_tournament, display_on_screen_players_tournament
-from vues.pairing import new_round, score_recording, display_on_screen_round
+from vues.pairing import new_round, display_on_screen_round
+from vues.scoring_matches import score_recording
 import sys
 #from vues import chessplayers, registration_tournament, registration_chessplayers_tourmanent, pairing
 
@@ -12,15 +13,14 @@ def start_chess():
     print("1.  Enregistrer un joueur dans la base de données du club.")
     print("2.  Afficher à l'écran la base de données des joueurs.")
     print("3.  Enregistrer un nouveau tournoi.")
-    print("4.  Afficher à l'écran la liste des tournois.")
-    print("5.  Afficher à l'écran les informations générales d'un tournoi.")
-    print("6.  Enregistrer un joueur dans un tournoi.")
-    print("7.  Afficher à l'écran la liste des joueurs d'un tournoi (par ordre alphabétique ou score)")
-    print("8.  Lancer une ronde d'un tournoi.")
-    print("9.  Enregistrer les scores des joueurs.")
-    print("10. Afficher à l'écran une ronde d'un tournoi.")
-    print("11. Afficher les informations générales d'une ronde.")
-    print("Ou  toutes autres touches du clavier, si vous souhaiter quitter le programme.")
+    print("4.  Afficher à l'écran la liste des tournois avec leurs informations générales.")
+    print("5.  Enregistrer un joueur dans un tournoi.")
+    print("6.  Afficher à l'écran la liste des joueurs d'un tournoi (par ordre alphabétique ou score)")
+    print("7.  Lancer une ronde d'un tournoi.")
+    print("8.  Enregistrer les scores des joueurs.")
+    print("9.  Afficher à l'écran une ronde d'un tournoi.")
+    print("10. Afficher les informations générales d'une ronde.")
+    print("Ou  toute autre touche du clavier, si vous souhaitez quitter le programme.")
     choose_action = input("\nVotre choix : ")
 
 
@@ -33,30 +33,27 @@ def start_chess():
             print("\nVous avez choisi d'afficher à l'écran la base de données des joueurs.\n")
             choice_action(display_on_screen_players_club)
         case "3":
-            print("\nVous avez choisi d\n")
+            print("\nVous avez choisi d'enregistrer un nouveau tournoi.\n")
             choice_action(register_tournament)
         case "4":
-            #print("\nVous avez choisi d\n")
-            pass
+            print("\nVous avez choisi d'afficher à l'écran la liste des tournois avec leurs informations générales.\n")
+            choice_action(display_on_screen_tournament_info)
         case "5":
-            #print("\nVous avez choisi d\n")
-            pass
-        case "6":
-            print("\nVous avez choisi d\n")
+            print("\nVous avez choisi d'enregistrer un joueur dans un tournoi\n")
             choice_action(register_players_tournament)
-        case "7":
-            print("\nVous avez choisi d\n")
+        case "6":
+            print("\nVous avez choisi d'afficher à l'écran la liste des joueurs d'un tournoi (par ordre alphabétique ou score)\n")
             choice_action(display_on_screen_players_tournament)
+        case "7":
+            print("\nVous avez choisi de lancer une ronde d'un tournoi\n")
+            choice_action(new_round)
         case "8":
             print("\nVous avez choisi d\n")
-            choice_action(new_round)
-        case "9":
-            print("\nVous avez choisi d\n")
             choice_action(score_recording)
-        case "10":
-            print("\nVous avez choisi d\n")
+        case "9":
+            print("\nVous avez choisi d'afficher à l'écran une ronde d'un tournoi.\n")
             choice_action(display_on_screen_round)
-        case "11":
+        case "10":
             #print("\nVous avez choisi d\n")
             pass
         case _:
@@ -72,7 +69,7 @@ def other_choice(action):
     print("\nQue souhaitez-vous faire? Pour cela, tapez le numéro corespondant à l'action")
     print("1. Refaire la même action?")
     print("2. Revenir au menu")
-    print("Ou  toutes autres touches du clavier, si vous souhaiter quitter le programme.")
+    print("Ou  toute autre touche du clavier, si vous souhaitez quitter le programme.")
     response = input("\nVotre choix : ")
 
     while response == "1" or "2":
@@ -82,7 +79,7 @@ def other_choice(action):
         elif response == "2":
             return start_chess()
         else :
-            print("Merci d'avoir utilisé ce programme. Keep on Chessing!")
+            print("Merci d'avoir utilisé ce programme. Keep on Chessing!\n")
             sys.exit(0)
     
 
@@ -98,15 +95,3 @@ start_chess()
 #
 #/root/gestion_tournoi_echecs/src/vues
 
-"""
-if choose_action =="1":
-    register_player()
-
-elif choose_action == "2":
-    display_on_screen_players_club()
-
-
-elif choose_action == "3":
-    
-
-elif choose_action == elif choose_action == elif choose_action == elif choose_action == elif choose_action == """
